e2c58012f424da59c24dda068ed568fb479137a1
ErikS <erik@artsymail.com>
2019-01-18 13:47:59 -0500

    wip

--- app/graphql/types/order_connection_with_total_count_type.rb
+++ app/graphql/types/order_connection_with_total_count_type.rb
@@ -2,8 +2,52 @@ class Types::OrderEdgeType < GraphQL::Types::Relay::BaseEdge
   node_type(Types::OrderInterface)
 end
 
+class PageCursorType < Types::BaseObject
+  field :cursor, String, 'first cursor on the page', null: false
+  field :isCurrent, Boolean, 'is this the current page?', null: false
+  field :page, Int, 'page number out of totalPages', null: false
+end
+
+class PageCursorsType < Types::BaseObject
+  field first: PageCursorType, 'optional, may be included in field `around`', null: true
+  field last: PageCursorType, 'optional, may be included in field `around`', null: true
+  field around: [PageCursorType], null: true
+
+  def first
+    {
+      cursor: 'woot',
+      isCurrent: false,
+      page: 1
+    }
+  end
+
+  def last
+    {
+      cursor: 'woot',
+      isCurrent: false,
+      page: 10
+    }
+  end
+
+  def around
+    [{
+      cursor: 'woot',
+      isCurrent: false,
+      page: 3
+    }, {
+      cursor: 'woot',
+      isCurrent: true,
+      page: 4
+    }, {
+      cursor: 'woot',
+      isCurrent: false,
+      page: 5
+    }]
+  end
+end
+
 class Types::OrderConnectionWithTotalCountType < GraphQL::Types::Relay::BaseConnection
-  implements Types::Pagination::PageableConnectionInterface
+  # implements Types::Pagination::PageableConnectionInterface
 
   edge_type(Types::OrderEdgeType)
 
@@ -13,4 +57,7 @@ class Types::OrderConnectionWithTotalCountType < GraphQL::Types::Relay::BaseConn
     # - `object.nodes` is the collection of Orders
     object.nodes.size
   end
+
+  field :page_cursors, PageCursorsType, 'Page cursors'
+
 end
